9ef7ad299edf8a249d0de25ce853f4b17e07960e
Dawid Stec <dawid.stec@gmail.com>
2018-11-25 14:59:11 +0100

    Added extended inmemoryrepository

--- src/main/java/pro/farmmanager/operation/OperationFacade.java
+++ src/main/java/pro/farmmanager/operation/OperationFacade.java
@@ -1,11 +1,20 @@
 package pro.farmmanager.operation;
 
-class OperationFacade {
+import pro.farmmanager.shared_kernel.Money;
+
+import java.util.UUID;
+
+public class OperationFacade {
 
     final private OperationManager operationManager;
 
     OperationFacade(OperationManager operationManager) {
         this.operationManager = operationManager;
+    }
+
+    public UUID createOperation(UUID farmlandId, OperationType type, Money cost) {
+        Operation operation = operationManager.createOperation(farmlandId, type, cost);
+        return operation.getId();
     }
 
 }
--- src/main/java/pro/farmmanager/operation/OperationManager.java
+++ src/main/java/pro/farmmanager/operation/OperationManager.java
@@ -1,11 +1,25 @@
 package pro.farmmanager.operation;
 
+import pro.farmmanager.farmlands.FarmlandFacade;
+import pro.farmmanager.farmlands.exceptions.FarmlandNotFoundException;
+import pro.farmmanager.shared_kernel.Money;
+
+import java.util.UUID;
+
 class OperationManager {
 
-    final private OperationRepository operationRepository;
+    private final OperationRepository operationRepository;
 
-    OperationManager(OperationRepository operationRepository) {
+    private final FarmlandFacade farmlandFacade;
+
+    OperationManager(OperationRepository operationRepository, FarmlandFacade farmlandFacade) {
         this.operationRepository = operationRepository;
+        this.farmlandFacade = farmlandFacade;
+    }
+
+    Operation createOperation(UUID farmlandId, OperationType type, Money cost) {
+            farmlandFacade.getFarmlandById(farmlandId);
+        return null;
     }
 
 }
