d16f4dae703a700d237f4cbdab427f10ac8a71d0
Dawid Stec <dawid.stec@gmail.com>
2018-11-25 13:58:45 +0100

    Made FarmlandFacade methods public

--- src/main/java/pro/farmmanager/farmlands/FarmlandFacade.java
+++ src/main/java/pro/farmmanager/farmlands/FarmlandFacade.java
@@ -14,19 +14,19 @@ public class FarmlandFacade {
         this.farmlandManager = farmlandManager;
     }
 
-    UUID createFarmland(String name, Float area, UUID ownerId) {
+    public UUID createFarmland(String name, Float area, UUID ownerId) {
         return farmlandManager.createFarmland(name, area, ownerId);
     }
 
-    FarmlandDto getFarmlandById(UUID farmlandId) {
+    public FarmlandDto getFarmlandById(UUID farmlandId) {
         return farmlandManager.findFarmlandById(farmlandId);
     }
 
-    void archiveFarmland(UUID farmlandId) {
+    public void archiveFarmland(UUID farmlandId) {
         farmlandManager.archiveFarmland(farmlandId);
     }
 
-    List<FarmlandDto> getFarmlandsForUser(UUID ownerId) {
+    public List<FarmlandDto> getFarmlandsForUser(UUID ownerId) {
         return farmlandManager.getFarmlandsForUser(ownerId)
                               .stream()
                               .map(Farmland::toDto)
